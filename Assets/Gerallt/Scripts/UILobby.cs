9667c68e0608758fef033194c238c1675d1db96b
Gerallt Franke <geralltf@gmail.com>
2022-03-15 15:27:43 +1100

    [Gerallt] - Lobby Tests

---
+++ Assets/Gerallt/Scripts/UILobby.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gerallt
+{
+    public class UILobby : MonoBehaviour
+    {
+        public GameObject JoinedClients;
+
+        public void JoinServer()
+        {
+            Debug.Log("Join server()");
+            
+            
+            
+        }
+        
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
