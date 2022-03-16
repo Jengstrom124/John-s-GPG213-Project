5b118766891cd3a1aafb7db8b68474c31e01bd07
Gerallt Franke <geralltf@gmail.com>
2022-03-16 11:21:30 +1100

    [Gerallt] - Simple Lobby UI.

--- Assets/Gerallt/Scripts/UILobby.cs
+++ Assets/Gerallt/Scripts/UILobby.cs
@@ -1,21 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Unity.Netcode;
 
 namespace Gerallt
 {
     public class UILobby : MonoBehaviour
     {
         public GameObject JoinedClients;
-
-        public void JoinServer()
+        public GameObject UIClientPrefab;
+        //public GeralltNetworkManager GeralltNetworkManager;
+        public ServerManager ServerManager;
+        
+        public void OnEnable()
         {
-            Debug.Log("Join server()");
-            
-            
-            
+            //GeralltNetworkManager.OnJoinServerEvent += OnJoinServer;
+            ServerManager.JoinServerEvent += OnJoinServer;
         }
         
+        public void OnDisable()
+        {
+            //GeralltNetworkManager.OnJoinServerEvent -= OnJoinServer;
+            ServerManager.JoinServerEvent -= OnJoinServer;
+        }
+        
+        private void OnJoinServer(int clientId)
+        {
+            UpdateClientsList();
+        }
+
+
+
+        public void UpdateClientsList()
+        {
+            // // Destroy all client UI instances in joined clients list
+            // foreach (Transform child in JoinedClients.transform)
+            // {
+            //     GameObject.Destroy(child);
+            // }
+            //
+            // // Refresh client UI with newly connected clients:
+            // //foreach (var client in NetworkManager.Singleton.ConnectedClients)
+            // foreach (var client in GeralltNetworkManager.NetworkedClientIds)
+            // {
+            //     var clientInstance = Instantiate(UIClientPrefab, JoinedClients.transform);
+            //     var tmp = clientInstance.GetComponent<TextMeshPro>();
+            //     //tmp.SetText(client.Value.ClientId.ToString());
+            //     tmp.SetText(client.ToString());
+            // }
+        }
         
         // Start is called before the first frame update
         void Start()
