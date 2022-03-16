0ba08c1e9ef70507c1bc96c566d8884413d44029
AaronMcDougall <1021572@student.sae.edu.au>
2022-03-16 11:07:48 +1100

    string, colour added

--- Assets/Tom/Scripts/PlayerController.cs
+++ Assets/Tom/Scripts/PlayerController.cs
@@ -1,8 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public CharacterBase selectedCharacter;
+
+    public string playerName;
+
+    public NetworkVariable<Color> playerColour;
 }
