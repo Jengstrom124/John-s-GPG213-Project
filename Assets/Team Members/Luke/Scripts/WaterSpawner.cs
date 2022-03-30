a84d477bf154cf43e624346e8364e35473e68a83
Luke Archbold <91643235+1024504@users.noreply.github.com>
2022-03-30 15:53:02 +1100

    Coral Spawner Working

--- Assets/Team Members/Luke/Scripts/WaterSpawner.cs
+++ Assets/Team Members/Luke/Scripts/WaterSpawner.cs
@@ -1,19 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WaterSpawner : MonoBehaviour
 {
 	public TerrainGenerator terrainGenerator;
+	public Terrain _terrain;
 	private TerrainData _terrainData;
+	public GameObject coralPrefab;
 
-	public int[,] coords;
-	// Start is called before the first frame update
-    
+	public float threshold = 0.45f;
+	public float frequency = 15f;
+	public int xOffset;
+	public int yOffset;
+
+	private IEnumerator Timer(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		Debug.Log("time");
+		FunctionLoop();
+	}
+
+	private void FunctionLoop()
+	{
+		for (int x = 0; x < terrainGenerator.width; x++)
+		{
+			for (int y = 0; y < terrainGenerator.height; y++)
+			{
+				SpawnCoral(x,y);
+			}
+		}
+	}
+	
+	private void SpawnCoral(int x, int y)
+	{
+		if (_terrainData.GetHeight(x, y) < 6)
+		{
+			float xCoord = (float) x / terrainGenerator.width;
+			float yCoord = (float) y / terrainGenerator.height;
+			float value = Mathf.PerlinNoise(xCoord * frequency + xOffset, yCoord * frequency + yOffset);
+			Debug.Log(value);
+			//if statement
+			if (value > threshold)
+			{
+				Vector3 worldPosition = new Vector3(x, _terrainData.GetHeight(x, y), y) + _terrain.transform.position;
+				Instantiate(coralPrefab, worldPosition, Quaternion.identity, transform);
+			}
+		}
+	}
+
 	void Start()
 	{
 		_terrainData = terrainGenerator.terrainDataForRandomExample;
-		coords = new int [10,10];
+		xOffset = Random.Range(-1000, 1000);
+		yOffset = Random.Range(-1000, 1000);
+
+	StartCoroutine(Timer(1f));
 	}
 
     // Update is called once per frame
