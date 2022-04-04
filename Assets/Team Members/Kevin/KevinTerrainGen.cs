85f5dd05ffc071c5d4a585453e34d75f2e64a8a2
SetearaKevinOm <1024325@student.sae.edu.au>
2022-04-04 22:52:53 +1000

    K

--- Assets/Team Members/Kevin/KevinTerrainGen.cs
+++ Assets/Team Members/Kevin/KevinTerrainGen.cs
@@ -2,32 +2,100 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Tree = Unity.VisualScripting.Antlr3.Runtime.Tree.Tree;
 
 public class KevinTerrainGen : MonoBehaviour
 {
     public TerrainGenerator terrainGenerator;
 
-    public TerrainData terrainData;
+    public Terrain terrainData;
+   
    
 
     public float frequencyX = 1f;
     public float frequencyY = 1f;
     public int yHeight = 20;
     
+    public float seaLevel = 10f; 
+    
     public GameObject sandPrefab;
-    public GameObject treePrefab; 
+    public GameObject treePrefab;
+    public GameObject seaweedPrefab;
+    public GameObject coralPrefab;
+    
+    private List<Vector3> currentPosition;
+    
     void Start()
     {
         terrainGenerator.calculateHeightCallback = CalculateHeightCallback;
         terrainGenerator.GenerateTerrain(terrainGenerator.terrainDataForRandomExample);
         terrainGenerator.depth = yHeight;
+        
+        
+        
+        for (int x = 0; x < terrainGenerator.width; x++)
+        {
+            for (int z = 0; z < terrainGenerator.height; z++)
+            {
+                float perlinValue1 = Mathf.PerlinNoise(10f * (float) x / terrainGenerator.width, 10f * (float) z / terrainGenerator.height);
+                Vector3 pos = new Vector3(x, 0, z);
+                pos.y = terrainData.SampleHeight(pos);
+                
+                if (pos.y > seaLevel)
+                {
+                    Instantiate(sandPrefab, pos,Quaternion.identity);
+                }
+                if (pos.y > seaLevel && perlinValue1 > 0.6f)
+                {
+                    Instantiate(treePrefab, pos,Quaternion.identity);
+                }
+                if (pos.y < seaLevel && perlinValue1 > 0.75)
+                {
+                    Instantiate(seaweedPrefab, pos,Quaternion.identity);
+                }
+
+                if (pos.y < seaLevel && perlinValue1 > 0.4f && perlinValue1 < 0.5f)
+                {
+                    Debug.Log(perlinValue1);
+                    Instantiate(coralPrefab, pos,Quaternion.identity);
+                }
+
+            }
+        }
     }
 
     private float CalculateHeightCallback(int x, int y)
     {
         float xCoord = (10f * (float) x / terrainGenerator.width);
         float yCoord = (10f * (float) y / terrainGenerator.height);
-        float perlinValue = Mathf.PerlinNoise(xCoord/frequencyX,yCoord/frequencyY);
+        
+        float perlinValue2 = Mathf.PerlinNoise(xCoord/frequencyX,yCoord/frequencyY);
+        
+        return perlinValue2; 
+    }
+}
+    
+    /*Vector3 pos = transform.position;
+     pos.y = Terrain.activeTerrain.SampleHeight(terrainGenerator.transform.position); 
+     
+     if (perlinValue > 0.9f)
+     {
+         float perlinValue2 = Mathf.PerlinNoise(xCoord/frequencyX,yCoord/frequencyY);
+         if (perlinValue2 > 0.9f)
+         {
+             Instantiate(sandPrefab, new Vector3(x,pos.y ,y),Quaternion.identity);
+             //Debug.Log(terrainGenerator.terrainDataForRandomExample.GetHeight(x,y));
+             Debug.Log(pos.y);
+         }
+     }
+
+     if (perlinValue is > 0.8f and < 0.9f)
+     {
+         Instantiate(treePrefab, new Vector3(x , pos.y, y),Quaternion.identity);
+         //Debug.Log(terrainGenerator.terrainDataForRandomExample.GetHeight(x,y));
+     }*/
+    
+        
         /*if (perlinValue > 0.9f)
         {
             float perlinValue2 = Mathf.PerlinNoise(xCoord/frequencyX,yCoord/frequencyY);
@@ -44,28 +112,27 @@ public class KevinTerrainGen : MonoBehaviour
            //Debug.Log(terrainGenerator.terrainDataForRandomExample.GetHeight(x,y));
         }*/
 
-        if (perlinValue > 0.9f)
+        /*if (perlinValue > 0.9f)
         {
             //Debug.Log(terrainGenerator.terrainDataForRandomExample.GetHeight(x, y));
         }
-        return perlinValue;
-    }
+        return perlinValue;*/
 
-    /*void SpawnSandObject()
-    {
-        for (int x = 0; x < terrainGenerator.width; x++)
+        /*void SpawnSandObject()
         {
-            Debug.Log(terrainGenerator.terrainDataForRandomExample.GetHeight(x, 0));
-            /*for (int y = 0; y < terrainGenerator.height; y++)
+            for (int x = 0; x < terrainGenerator.width; x++)
             {
-                Vector3 pos = terrainGenerator.transform.position;
-                pos.y = terrainGenerator.terrainDataForRandomExample.GetHeight(x, y);
-                Debug.Log(terrainGenerator.terrainDataForRandomExample.GetHeight(x, y));
-                //Instantiate(sandPrefab, new Vector3(x, pos.y, y), Quaternion.identity);
-            }#1#
-        }
-
-    }*/
+                Debug.Log(terrainGenerator.terrainDataForRandomExample.GetHeight(x, 0));
+                /*for (int y = 0; y < terrainGenerator.height; y++)
+                {
+                    Vector3 pos = terrainGenerator.transform.position;
+                    pos.y = terrainGenerator.terrainDataForRandomExample.GetHeight(x, y);
+                    Debug.Log(terrainGenerator.terrainDataForRandomExample.GetHeight(x, y));
+                    //Instantiate(sandPrefab, new Vector3(x, pos.y, y), Quaternion.identity);
+                }#1#
+            }
+    
+        }*/
 
 
     /*for (int i = 0; i < terrainGenerator.width; i++)
@@ -91,4 +158,4 @@ public class KevinTerrainGen : MonoBehaviour
         }
     }*/
 
-}
+
