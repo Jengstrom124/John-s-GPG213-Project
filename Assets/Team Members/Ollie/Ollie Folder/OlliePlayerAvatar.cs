b9fa6b6f86ab6b4d300b3766facb93b5f0aec18f
OllieSAE <1020925@student.sae.edu.au>
2022-04-06 11:35:54 +1000

    added fish pack
    fish go brr

---
+++ Assets/Team Members/Ollie/Ollie Folder/OlliePlayerAvatar.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class OlliePlayerAvatar : MonoBehaviour
+{
+    public bool grounded;
+    public Rigidbody rb;
+    public float forwardSpeed;
+    public float turnSpeed;
+    private Vector3 pos;
+
+    public delegate void TouchingCar(GameObject target);
+    public event TouchingCar touchingCarEvent;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        forwardSpeed = 10;
+        turnSpeed = 3f;
+        grounded = true;
+    }
+
+    private void Update()
+    {
+        pos = transform.position;
+    }
+
+    public void Forward()
+    {
+        if (grounded)
+        {
+            rb.AddRelativeForce(0,0,forwardSpeed);
+        }
+    }
+
+    public void Backward()
+    {
+        if (grounded)
+        {
+            rb.AddRelativeForce(0,0,-forwardSpeed);
+        }
+    }
+
+    public void Left()
+    {
+        if (grounded)
+        {
+            rb.AddRelativeTorque(0, -turnSpeed,0);
+        }
+    }
+    
+    public void Right()
+    {
+        if (grounded)
+        {
+            rb.AddRelativeTorque(0, turnSpeed,0);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.GetComponent<OllieVehicleBase>() != null)
+        {
+            touchingCarEvent?.Invoke(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Team Members/Ollie/Ollie Folder/RectangleCar.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectangleCar : OllieVehicleBase
+{
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        forwardSpeed = 20;
+        turnSpeed = 2;
+        grounded = true;
+        car = this.gameObject;
+    }
+}
