c0361d913bb813d84ad114c432260c05a3341986
Gerallt Franke <geralltf@gmail.com>
2022-04-13 12:20:46 +1000

    FIX: Added null check to controllable

--- Assets/Team Members/Tom/Scripts/PlayerController.cs
+++ Assets/Team Members/Tom/Scripts/PlayerController.cs
@@ -25,41 +25,44 @@ public class PlayerController : MonoBehaviour
             // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
             IControllable controllable = controlledThing.GetComponentInChildren<IControllable>();
 
-            controllable.Steer(0f);
-            if (InputSystem.GetDevice<Keyboard>().aKey.wasPressedThisFrame)
+            if (controllable != null)
             {
-                // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
-                controllable.Steer(-1f);
+                controllable.Steer(0f);
+                if (InputSystem.GetDevice<Keyboard>().aKey.wasPressedThisFrame)
+                {
+                    // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
+                    controllable.Steer(-1f);
+                }
+                if (InputSystem.GetDevice<Keyboard>().dKey.wasPressedThisFrame)
+                {
+                    // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
+                    controllable.Steer(1f);
+                }
+                if (InputSystem.GetDevice<Keyboard>().wKey.wasPressedThisFrame)
+                {
+                    // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
+                    controllable.Accelerate(1f);
+                }
+                if (InputSystem.GetDevice<Keyboard>().sKey.wasPressedThisFrame)
+                {
+                    // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
+                    controllable.Reverse(1f);
+                }
+                if (InputSystem.GetDevice<Keyboard>().fKey.wasPressedThisFrame)
+                {
+                    // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
+                    controllable.Action();
+                }
+                if (InputSystem.GetDevice<Keyboard>().eKey.wasPressedThisFrame)
+                {
+                    // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
+                    controllable.Action2();
+                }
+                if (InputSystem.GetDevice<Keyboard>().qKey.wasPressedThisFrame)
+                {
+                    // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
+                    controllable.Action3();
+                }   
             }
-            if (InputSystem.GetDevice<Keyboard>().dKey.wasPressedThisFrame)
-            {
-                // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
-                controllable.Steer(1f);
-            }
-            if (InputSystem.GetDevice<Keyboard>().wKey.wasPressedThisFrame)
-            {
-                // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
-                controllable.Accelerate(1f);
-            }
-            if (InputSystem.GetDevice<Keyboard>().sKey.wasPressedThisFrame)
-            {
-                // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
-                controllable.Reverse(1f);
-            }
-            if (InputSystem.GetDevice<Keyboard>().fKey.wasPressedThisFrame)
-            {
-                // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
-                controllable.Action();
-            }
-            if (InputSystem.GetDevice<Keyboard>().eKey.wasPressedThisFrame)
-            {
-                // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
-                controllable.Action2();
-            }
-            if (InputSystem.GetDevice<Keyboard>().qKey.wasPressedThisFrame)
-            {
-                // Can't drag interfaces directly in the inspector, so get at it from a component/GameObject reference instead
-                controllable.Action3();
-            }        
         }
 }
