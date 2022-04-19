e2f8f650d06174c52f9b22dbee17235b0e813035
Luke Archbold <1024504@student.sae.edu.au>
2022-04-19 20:13:27 +1000

    Luke Eagle Update
    A little janky turning around between bezier curves, but working well otherwise

---
+++ Assets/Team Members/Luke/Scripts/InAirSpawner.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class InAirSpawner : MonoBehaviour
+{
+	public TerrainGenerator terrainGenerator;
+	/*public Terrain _terrain;*/
+	private TerrainData _terrainData;
+	public List<GameObject> birdPrefabs;
+
+	public float threshold = 0.9f;
+	public float frequency = 30f;
+	public int xOffset;
+	public int yOffset;
+	public float fringe;
+	
+
+	private void FunctionLoop()
+	{
+		for (int x = 0; x < terrainGenerator.width; x++)
+		{
+			for (int y = 0; y < terrainGenerator.height; y++)
+			{
+				SpawnTrees(x,y);
+			}
+		}
+	}
+	
+	private void SpawnTrees(int x, int y)
+	{
+		if (_terrainData.GetHeight(x, y) > 12f)
+		{
+			float xCoord = (float) x / terrainGenerator.width;
+			float yCoord = (float) y / terrainGenerator.height;
+			if (!(xCoord < fringe || xCoord > 1 - fringe || yCoord < fringe || yCoord > 1 - fringe))
+			{
+				float value = Mathf.PerlinNoise(xCoord * frequency + xOffset, yCoord * frequency + yOffset);
+				if (value > threshold)
+				{
+					GameObject go;
+					Vector3 worldPosition = new Vector3(x, 15f, y)/* + _terrain.transform.position*/;
+					if (Random.Range(0f, 1f) > 0.5f)
+					{
+						go = Instantiate(birdPrefabs[Random.Range(0, birdPrefabs.Count)], worldPosition, Quaternion.Euler(0, Random.Range(-179, 180),0), transform);
+						LukeEagle eagleScript = go.GetComponent<LukeEagle>();
+					}
+				}
+			}
+		}
+	}
+
+	void Awake()
+	{
+		GetComponentInParent<LukeTerrain>().FinishSpawningEvent += MyStart;
+	}
+	
+	private void MyStart()
+	{
+		_terrainData = terrainGenerator.terrainDataForRandomExample;
+		xOffset = Random.Range(-1000, 1000);
+		yOffset = Random.Range(-1000, 1000);
+		FunctionLoop();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+        
+	}
+}
+
--- Assets/Team Members/Luke/Scripts/LukeEagle.cs
+++ Assets/Team Members/Luke/Scripts/LukeEagle.cs
@@ -1,19 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using UnityEditor.Animations;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LukeEagle : MonoBehaviour
 {
 	public bool circuitBreaker;
-	public List<Vector3> controlPoints = new (2);
+	public List<Vector3> controlPoints = new(4);
+	public int iterationsPerCourse = 200;
+	public float durationPerCourse = 7f;
+	public float rangePerCourse = 25f;
+	private float progress;
+	public Vector3 terrainPosition;
+	public Vector3 terrainDimensions;
 
-	public Vector3 BezierFunction(List<Vector3> points, float t) 
+	public Vector3 BezierFunction(List<Vector3> points, float t)
 	{
 		List<Vector3> a = points;
-		List<Vector3> b = new List<Vector3>(points.Count-1);
-		
-		for (var i = 0; i < points.Count-1; i++)
+		List<Vector3> b = new List<Vector3>(points.Count - 1);
+
+		for (var i = 0; i < points.Count - 1; i++)
 		{
 			for (var j = 0; j < b.Capacity; j++)
 			{
@@ -26,41 +35,108 @@ public class LukeEagle : MonoBehaviour
 				b = new List<Vector3>(a.Count - 1);
 			}
 		}
-
-		//Debug.Log(a[0]);
 		return a[0];
+	}
+
+	private void BezierLooper()
+	{
+		if (progress > 0)
+		{
+			StartCoroutine(RunBezier());
+		}
+		else
+		{
+			StartCoroutine(BetweenCourses());
+		}
 	}
 
 	private IEnumerator RunBezier()
 	{
-		//transform.position = BezierFunction();
-		
-		
-		yield return new WaitForSeconds(0.1f);
-		if(!circuitBreaker)
-			RunBezier();
+		if (circuitBreaker)
+		{
+			yield break;
+		}
+
+		Vector3 tempPosition1 = transform.position;
+		Vector3 tempPosition2 = BezierFunction(controlPoints, progress);
+		transform.DOMove(tempPosition2, durationPerCourse / iterationsPerCourse);
+		progress += 1f / iterationsPerCourse;
+		TurnBody(tempPosition1, tempPosition2);
+		yield return new WaitForSeconds(durationPerCourse / iterationsPerCourse);
+		if (progress >= 1f)
+		{
+			progress = 0f;
+			RandomizeBezierPoints();
+		}
+
+		BezierLooper();
+	}
+
+	private IEnumerator BetweenCourses()
+	{
+		float angle = Vector3.SignedAngle(transform.position, BezierFunction(controlPoints, 1f / iterationsPerCourse) - transform.position, Vector3.up);
+		transform.DORotate(new Vector3(0, angle, 0), durationPerCourse / 5f);
+		yield return new WaitForSeconds(durationPerCourse / 5f);
+		StartCoroutine(RunBezier());
 	}
 	
+	private IEnumerator RandomStartTimer()
+	{
+		yield return new WaitForSeconds(Random.Range(0f, 2f));
+		StartCoroutine(RunBezier());
+	}
+
+	//smoothing by dividing up into segments of similar length
+
+
 	private void TurnBody(Vector3 p1, Vector3 p2)
 	{
-		Vector3 angles = transform.eulerAngles;
-		//angles = new Vector3(angles.x, , angles.z);
+		Vector3 heading = p2 - p1;
+		transform.rotation = Quaternion.LookRotation(heading, Vector3.up);
+	}
+
+	private void InitializeBezierPoints(List<Vector3> cPs)
+	{
+		Vector3 position = transform.position;
+		cPs.Add(position);
+		for (int i = 1; i < cPs.Capacity; i++)
+		{
+			float controlX = terrainPosition.x + Random.Range(0f, terrainDimensions.x);
+			float controlY = terrainPosition.y + 15f;
+			float controlZ = terrainPosition.z + Random.Range(0f, terrainDimensions.z);
+
+			controlX = position.x + Mathf.Clamp(controlX - position.x, -rangePerCourse, rangePerCourse);
+			controlZ = position.z + Mathf.Clamp(controlZ - position.z, -rangePerCourse, rangePerCourse);
+
+			cPs.Add(new Vector3(controlX, controlY, controlZ));
+		}
 	}
 
 	private void RandomizeBezierPoints()
 	{
-		
-	}
-	
-	// Start is called before the first frame update
-    void Start()
-    {
-	    StartCoroutine(RunBezier());
-    }
+		Vector3 position = transform.position;
+		controlPoints[0] = controlPoints[^1];
+		controlPoints[1] = controlPoints[0] + transform.TransformDirection(new Vector3(Random.Range(-5f, 5f), 0f, 5f));
+		for (int i = 2; i < controlPoints.Count; i++)
+		{
+			float controlX = terrainPosition.x + Random.Range(0f, terrainDimensions.x);
+			float controlY = terrainPosition.y + 15f;
+			float controlZ = terrainPosition.z + Random.Range(0f, terrainDimensions.z);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+			controlX = position.x + Mathf.Clamp(controlX - position.x, -rangePerCourse, rangePerCourse);
+			controlZ = position.z + Mathf.Clamp(controlZ - position.z, -rangePerCourse, rangePerCourse);
+
+			controlPoints[i] = new Vector3(controlX, controlY, controlZ);
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		TerrainGenerator a = GetComponentInParent<InAirSpawner>().terrainGenerator;
+		terrainDimensions = new Vector3(a.width, a.depth, a.height);
+		InitializeBezierPoints(controlPoints);
+		RandomizeBezierPoints();
+		StartCoroutine(RandomStartTimer());
+	}
 }
