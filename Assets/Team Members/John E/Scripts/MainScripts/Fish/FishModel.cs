933290ceb175bbddfc599996266f37f18c7feedf
Jengstrom124 <87103784+Jengstrom124@users.noreply.github.com>
2022-05-04 10:07:22 +1000

    Revert "Revert "Merge branch 'main' of https://github.com/Jengstrom124/John-s-GPG213-Project""
    This reverts commit c59adb8e963265b10a53e4f4c6396b3bc0e6f416.

--- Assets/Team Members/John E/Scripts/MainScripts/Fish/FishModel.cs
+++ Assets/Team Members/John E/Scripts/MainScripts/Fish/FishModel.cs
@@ -7,8 +7,12 @@ public class FishModel : FishBase, IRTS
 {
     public event Action<bool> onPlayerFishEvent;
 
+    public float alignForceTimer = 2f;
+    float defaultAlignForce;
+
     PathTracker pathTracker;
     Align align;
+
     private void Awake()
     {
         pathTracker = GetComponent<PathTracker>();
@@ -17,11 +21,13 @@ public class FishModel : FishBase, IRTS
     private void Start()
     {
         pathTracker.destinationReachedEvent += AtDestinationReaction;
+        defaultAlignForce = align.force;
     }
 
     void AtDestinationReaction()
     {
         Debug.Log("MADE IT!! - return to being a fish");
+        Deselected();
     }
 
     public void Selected()
@@ -32,10 +38,20 @@ public class FishModel : FishBase, IRTS
     public void Deselected()
     {
         onPlayerFishEvent?.Invoke(false);
+
+        align.force = defaultAlignForce;
+
+        //Reset Pathfinding
+        pathTracker.ResetPathTracking();
+
+        //HACK - need to reset the fish variable in the controller
+        John.JohnRTSTestController.Instance.ResetController();
     }
 
     public void SetDestination(Vector3 position)
     {
         pathTracker.GetPathToDestination(position);
+
+        Mathf.Lerp(align.force, 0, alignForceTimer * Time.deltaTime);
     }
 }
