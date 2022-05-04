607ba3497088b2234f0657e94cce36c133e61c4e
Ollie Vincent <1020925@student.sae.edu.au>
2022-05-04 13:03:16 +1000

    dolphin now eats fish and grows
    dotween pulse in size

--- Assets/Team Members/Ollie/Ollie Scripts/Stomach.cs
+++ Assets/Team Members/Ollie/Ollie Scripts/Stomach.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Stomach : MonoBehaviour
 {
+    public FishContainer fishContainer;
     public IPredator iPredator;
-    
+    public GameObject dolphin;
+    private Vector3 originalScale;
+
+    public void Start()
+    {
+        originalScale = dolphin.transform.localScale;
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         IEdible edible = other.GetComponent<IEdible>();
@@ -16,10 +26,16 @@ public class Stomach : MonoBehaviour
 
             if (edible.GetInfo().edibleType == EdibleType.Food)
             {
-                // increase scale
-                // add fish to stomach (container)
-                print("nomnomnom");
+                fishContainer.AddToStomach(other.GetComponent<FishBase>());
+                dolphin.transform.DOPunchScale(new Vector3(0.5f, 0.5f, 0.5f), 0.75f, 1, 0.2f);
+                StartCoroutine(EatScaleDelayCoroutine());
             }
         }
+    }
+
+    private IEnumerator EatScaleDelayCoroutine()
+    {
+        yield return new WaitForSeconds(0.75f);
+        dolphin.transform.DOScale(originalScale, 0.5f);
     }
 }
